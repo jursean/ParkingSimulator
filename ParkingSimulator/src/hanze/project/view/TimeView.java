684d887309a646c224f1f8c5121e4b213579b956
jursean <35691780+jursean@users.noreply.github.com>
2018-01-29 18:59:22 +0100

    Update 2.81 TimeView
    Nieuwe labels

--- ParkingSimulator/src/hanze/project/view/TimeView.java
+++ ParkingSimulator/src/hanze/project/view/TimeView.java
@@ -17,10 +17,10 @@ public class TimeView extends AbstractView{
     private JLabel days;
     private JLabel simulationTime;
     private JLabel startTimeString;
-    private JLabel weekend;
-    private JLabel weekendFalse;
-    private JLabel weekendTrue;
-    private Dimension size;
+    private JLabel voorstelling;
+    private JLabel geenVoorstelling;
+    private JLabel koopAvond;
+    private JLabel geenKoopAvond;
 
     // CONSTRUCTORS
 
@@ -28,7 +28,7 @@ public class TimeView extends AbstractView{
         super(timeController);
         Time time = (Time) super.model;
 
-        Dimension size = new Dimension(250, 400);
+        //Dimension size = new Dimension(250, 400);
 
         //Label initialiseren
         this.dateLabel = new JLabel("Tijd");
@@ -37,6 +37,10 @@ public class TimeView extends AbstractView{
         this.days = new JLabel("Dagen: ");
         this.simulationTime = new JLabel();
         this.startTimeString = new JLabel();
+        this.voorstelling = new JLabel("Er is een Voorstelling");
+        this.geenVoorstelling = new JLabel("Geen Voorstelling");
+        this.koopAvond = new JLabel("Er is Koopavond");
+        this.geenKoopAvond = new JLabel("Geen Koopavond");
 
         //Zetten van de bounds
         dateLabel.setBounds(150,0,100,30); // Default = x: 3 y: -1 width: 100 height: 30
@@ -45,6 +49,17 @@ public class TimeView extends AbstractView{
         days.setBounds(5,60,100,30);
         simulationTime.setBounds(5,80,100,30);
         startTimeString.setBounds(5,100,100,30);
+        voorstelling.setBounds(5,120,100,30);
+        geenVoorstelling.setBounds(5,120,100,30);
+        koopAvond.setBounds(5,140,100,30);
+        geenKoopAvond.setBounds(5,140,100,30);
+
+        //Zetten van label kleuren
+        voorstelling.setForeground(Color.green);
+        geenVoorstelling.setForeground(Color.red);
+        koopAvond.setForeground(Color.green);
+        geenKoopAvond.setForeground(Color.red);
+
 
         //Label toevoegen
         add(dateLabel);
@@ -53,6 +68,10 @@ public class TimeView extends AbstractView{
         add(days);
         add(simulationTime);
         add(startTimeString);
+        add(voorstelling);
+        add(geenVoorstelling);
+        add(koopAvond);
+        add(geenKoopAvond);
     }
 
     public void updateView(){
@@ -65,15 +84,24 @@ public class TimeView extends AbstractView{
         hours.setText(("Uren: ")+time.getRunningHours());
         days.setText(("Dagen: ")+time.getRunningDays());
 
-        /*if (time.isWeekend()) {
-            weekendFalse.setVisible(false);
-            weekendTrue.setVisible(true);
+        if (time.isVoorstelling()) {
+            voorstelling.setVisible(true);
+            geenVoorstelling.setVisible(false);
         }
         else {
-            weekendTrue.setVisible(false);
-            weekendFalse.setVisible(true);
+            voorstelling.setVisible(false);
+            geenVoorstelling.setVisible(true);
         }
-        */
+
+        if (time.isKoopAvond()){
+            koopAvond.setVisible(true);
+            geenKoopAvond.setVisible(false);
+        }
+        else {
+            koopAvond.setVisible(false);
+            geenKoopAvond.setVisible(true);
+        }
+
 
         setVisible(true);
         super.updateView();
