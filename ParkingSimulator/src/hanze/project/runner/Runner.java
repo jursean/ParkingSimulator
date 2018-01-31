58400ec4f67c28768c533542e91e733f5b481a13
timboy6699 <35691577+timboy6699@users.noreply.github.com>
2018-01-31 12:09:27 +0100

    javadoc

--- ParkingSimulator/src/hanze/project/runner/Runner.java
+++ ParkingSimulator/src/hanze/project/runner/Runner.java
@@ -1,14 +1,25 @@
 package hanze.project.runner;
 
-import hanze.project.main.ParkingSimulator;// CLASS
+import hanze.project.main.ParkingSimulator;
+
+/**
+ * Class Runner
+ * Deze klasse zorgt ervoor dat de simulator word uitgevoerd.
+ *
+ * @author Jurian de Vries, Sebastiaan ter Veen, Deni Grabic, Tim Gorter, Sander Steenbergen
+ * @version 31-01-2018
+ */
 
 public class Runner {
 
-    // METHODS
+    // De methodes
+
+    /**
+     * Zorgt ervoor dat de simulator uitgevoerd word.
+     * @param args De argumenten
+     */
 
     public static void main(String[] args){
-
-        // Voert de constructor uit in Main
         new ParkingSimulator();
     }
 
