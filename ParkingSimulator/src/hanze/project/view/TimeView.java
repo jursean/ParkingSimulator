5fe1a411c25a0221eebe110a47319aaf57c03ec6
jursean <35691780+jursean@users.noreply.github.com>
2018-01-31 18:53:59 +0100

    Update 3.0 TimeView

--- ParkingSimulator/src/hanze/project/view/TimeView.java
+++ ParkingSimulator/src/hanze/project/view/TimeView.java
@@ -25,9 +25,10 @@ public class TimeView extends AbstractView{
     private JLabel simulationTime;
     private JLabel startTimeString;
     private JLabel weekend;
-    private JLabel weekendFalse;
-    private JLabel weekendTrue;
-    private Dimension size;
+    private JLabel voorstelling;
+    private JLabel koopavond;
+    private JLabel avond;
+    private JLabel nacht;
 
     // De constructors
 
@@ -35,31 +36,44 @@ public class TimeView extends AbstractView{
         super(timeController);
         Time time = (Time) super.model;
 
-        Dimension size = new Dimension(250, 400);
-
         //Label maken
         this.dateLabel = new JLabel("Tijd");
-        this.minutes = new JLabel("Minuten: ");
-        this.hours = new JLabel("Uren: ");
-        this.days = new JLabel("Dagen: ");
-        this.simulationTime = new JLabel();
-        this.startTimeString = new JLabel();
+        this.startTimeString = new JLabel("Start datum van de simulatie: ");
+        this.simulationTime = new JLabel("Huidige datum/tijd: ");
+        this.minutes = new JLabel("Minuten: 0");
+        this.hours = new JLabel("Uren: 0");
+        this.days = new JLabel("Dagen: 0");
+        this.weekend = new JLabel("Weekend");
+        this.voorstelling = new JLabel("Voorstelling");
+        this.koopavond = new JLabel("Koopavond");
+        this.avond = new JLabel("Avond");
+        this.nacht = new JLabel("Nacht");
 
         //Instellen van de grenswaarde
         dateLabel.setBounds(150,0,100,30); // Default = x: 3 y: -1 width: 100 height: 30
-        minutes.setBounds(5,20,100,30);
-        hours.setBounds(5,40,100,30);
-        days.setBounds(5,60,100,30);
-        simulationTime.setBounds(5,80,100,30);
-        startTimeString.setBounds(5,100,100,30);
+        startTimeString.setBounds(5,20,100,30);
+        simulationTime.setBounds(5,60,100,30);
+        minutes.setBounds(5,80,100,30);
+        hours.setBounds(5,100,100,30);
+        days.setBounds(5,120,100,30);
+        weekend.setBounds(5,160,100,30);
+        voorstelling.setBounds(5,180,100,30);
+        koopavond.setBounds(5,200,100,30);
+        avond.setBounds(5,220,100,30);
+        nacht.setBounds(5,240,100,30);
 
         //De labels toevoegen
         add(dateLabel);
+        add(startTimeString);
+        add(simulationTime);
         add(minutes);
         add(hours);
         add(days);
-        add(simulationTime);
-        add(startTimeString);
+        add(weekend);
+        add(voorstelling);
+        add(koopavond);
+        add(avond);
+        add(nacht);
     }
 
     // De methodes
@@ -71,22 +85,44 @@ public class TimeView extends AbstractView{
     public void updateView(){
         Time time = (Time) super.model;
 
-        simulationTime.setText(time.getCurrentTime());
         startTimeString.setText(time.getStartTime());
 
+        simulationTime.setText(time.getCurrentTime());
         minutes.setText(("Minuten: ")+time.getRunningMinutes());
         hours.setText(("Uren: ")+time.getRunningHours());
         days.setText(("Dagen: ")+time.getRunningDays());
 
-        /*if (time.isWeekend()) {
-            weekendFalse.setVisible(false);
-            weekendTrue.setVisible(true);
+        if (time.isWeekend()) {
+            weekend.setForeground(Color.green);
         }
         else {
-            weekendTrue.setVisible(false);
-            weekendFalse.setVisible(true);
+            weekend.setForeground(Color.red);
         }
-        */
+
+        if (time.isVoorstelling()){
+            voorstelling.setForeground(Color.green);
+        }else{
+            voorstelling.setForeground(Color.red);
+        }
+
+        if (time.isKoopAvond()){
+            koopavond.setForeground(Color.green);
+        }else{
+            koopavond.setForeground(Color.red);
+        }
+
+        if (time.isAvond()){
+            avond.setForeground(Color.green);
+        }else{
+            avond.setForeground(Color.red);
+        }
+
+        if (time.isNacht()){
+            nacht.setForeground(Color.green);
+        }else{
+            nacht.setForeground(Color.red);
+        }
+
 
         setVisible(true);
         super.updateView();
