cba232d0ebbac0e4e25378fdabc812b13daefd42
denigrabic <35691682+denigrabic@users.noreply.github.com>
2018-01-31 12:32:49 +0100

    Update LegendView.java
    Legenda bewerkt , gekleurde blokjes toegevoegd

--- ParkingSimulator/src/hanze/project/view/LegendView.java
+++ ParkingSimulator/src/hanze/project/view/LegendView.java
@@ -15,8 +15,7 @@ import java.awt.*;
 
 public class LegendView extends AbstractView {
 
-    //De velden
-
+    //Initialiseer de velden voor de beschrijving van de kleuren
     private JLabel legendaKopje;
     private JLabel wit;
     private JLabel blauw;
@@ -24,30 +23,57 @@ public class LegendView extends AbstractView {
     private JLabel groen;
     private JLabel grijs;
 
-    //De constructors
+    /**
+     * Deze methode zorgt voor de gekleurde vakjes in de legenda
+     * @param a
+     */
+    public void paintComponent(Graphics a) {
+        super.paintComponent(a);
+
+        a.setColor(Color.white);
+        a.fillRect(20,52,20,8);
+
+
+        a.setColor(Color.BLUE);
+        a.fillRect(20,72,20,8);
+
+
+        a.setColor(Color.red);
+        a.fillRect(20,92,20,8);
+
+
+        a.setColor(Color.GREEN);
+        a.fillRect(20,112,20,8);
+
+
+        a.setColor(Color.darkGray);
+        a.fillRect(20,132,20,8);
+    }
 
     public LegendView(Model simulator) {
         super(simulator);
         Dimension size = new Dimension(250, 400);
 
         this.legendaKopje = new JLabel("Legenda");
-        this.wit = new JLabel("Wit = Vrije parkeerplek");
-        this.blauw = new JLabel("Blauw = Parkeerder met abbonement");
-        this.rood = new JLabel("Rood = Parkeerder zonder abbonement");
-        this.groen = new JLabel("Groen = Gereserveerde parkeerplek");
-        this.grijs = new JLabel("Grijs = Bezette gereserveerde parkeerplek");
+        this.wit = new JLabel("= Vrije parkeerplek ");
+        this.blauw = new JLabel("= Parkeerder met abbonement");
+        this.rood = new JLabel("= Parkeerder zonder abbonement");
+        this.groen = new JLabel("= Gereserveerde parkeerplek");
+        this.grijs = new JLabel("= Bezette gereserveerde parkeerplek");
 
-        legendaKopje.setBounds(90, 0, 100, 30);
-        wit.setBounds(5, 20, 400, 30);
-        blauw.setBounds(5, 40, 400, 30);
-        rood.setBounds(5, 60, 400, 30);
-        groen.setBounds(5, 80, 400, 30);
-        grijs.setBounds(5, 100, 400, 30);
+
+        legendaKopje.setBounds(120, 0, 100, 30);
+        wit.setBounds(50, 40, 800, 30);
+        blauw.setBounds(50, 60, 400, 30);
+        rood.setBounds(50, 80, 400, 30);
+        groen.setBounds(50, 100, 400, 30);
+        grijs.setBounds(50, 120, 400, 30);
+
 
         wit.setForeground(Color.black);
-        blauw.setForeground(Color.blue);
-        rood.setForeground(Color.red);
-        groen.setForeground(Color.green);
+        blauw.setForeground(Color.black);
+        rood.setForeground(Color.black);
+        groen.setForeground(Color.black);
 
         add(legendaKopje);
         add(wit);
