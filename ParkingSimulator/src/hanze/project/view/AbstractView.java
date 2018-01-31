73de9476af97984accf8f79bd072e8071c663a25
timboy6699 <35691577+timboy6699@users.noreply.github.com>
2018-01-31 12:10:27 +0100

    javadoc

--- ParkingSimulator/src/hanze/project/view/AbstractView.java
+++ ParkingSimulator/src/hanze/project/view/AbstractView.java
@@ -3,18 +3,22 @@ package hanze.project.view;
 import hanze.project.logic.AbstractModel;
 import javax.swing.*;
 
-// CLASS
+/**
+ * Class AbstractView
+ * ??????????????????????????????????????????????????????????????????????
+ *
+ * @author Jurian de Vries, Sebastiaan ter Veen, Deni Grabic, Tim Gorter, Sander Steenbergen
+ * @version 31-01-2018
+ */
 
 public class AbstractView extends JPanel {
 
-    // FIELDS
+    // De velden
 
     public AbstractModel model;
 
-    // CONSTRUCTORS
+    // De constructors
 
-    //  Constructor of AbstractView that expects a logic belonging to this view.
-    //  @param model AbstractModel that belongs to this view.
     public AbstractView(AbstractModel model) {
         this.model = model;
         model.addView(this);
@@ -32,6 +36,10 @@ public class AbstractView extends JPanel {
 //    public AbstractModel getModel() {
 //        return model;
 //    }
+
+    /**
+     * Update de view
+     */
 
     public void updateView() {
         repaint();
