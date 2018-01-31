20a0cc93ee78f50a42508de93b81c5d5ade1030c
timboy6699 <35691577+timboy6699@users.noreply.github.com>
2018-01-31 12:03:22 +0100

    javadoc

--- ParkingSimulator/src/hanze/project/logic/Location.java
+++ ParkingSimulator/src/hanze/project/logic/Location.java
@@ -1,31 +1,37 @@
 package hanze.project.logic;
 
-// CLASS
+/**
+ * Class Location
+ * Deze klasse stelt de grootte van de parkeergarage in.
+ *
+ * @author Jurian de Vries, Sebastiaan ter Veen, Deni Grabic, Tim Gorter, Sander Steenbergen
+ * @version 31-01-2018
+ */
 
 public class Location {
 
-    // FIELDS
+    // De velden
 
     private int floor;
     private int row;
     private int place;
 
-    // CONSTRUCTORS
+    // De constructors
 
-    /**
-     * Constructor for objects of class Location
-     */
     public Location(int floor, int row, int place) {
         this.floor = floor;
         this.row = row;
         this.place = place;
     }
 
-    // METHODS
+    // De methodes
 
     /**
-     * Implement content equality.
+     * Checked of een auto op een bepaalde locatie staat.
+     * @param obj objecten die vergeleken moeten worden
+     * @return boolean
      */
+
     public boolean equals(Object obj) {
         if(obj instanceof Location) {
             Location other = (Location) obj;
@@ -37,39 +43,42 @@ public class Location {
     }
 
     /**
-     * Return a string of the form floor,row,place.
-     * @return A string representation of the location.
+     * Zet de locatie om naar een string met verdieping, rij en plaats.
+     * @return string Met de locatie van de auto
      */
+
     public String toString() {
         return floor + "," + row + "," + place;
     }
 
     /**
-     * Use the 10 bits for each of the floor, row and place
-     * values. Except for very big car parks, this should give
-     * a unique hash code for each (floor, row, place) tupel.
-     * @return A hashcode for the location.
+     * Geeft een 10 bit hashcode met daarin de locatie.
+     * @return hashcode Met de locatie
      */
+
     public int hashCode() {
         return (floor << 20) + (row << 10) + place;
     }
 
     /**
-     * @return The floor.
+     * Geeft de verdieping terug.
+     * @return floor Verdieping
      */
     public int getFloor() {
         return floor;
     }
 
     /**
-     * @return The row.
+     * Geeft de rij terug.
+     * @return row Rij
      */
     public int getRow() {
         return row;
     }
 
     /**
-     * @return The place.
+     * Geeft de de plek terug.
+     * @return place Plek
      */
     public int getPlace() {
         return place;
