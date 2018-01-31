7a0ee7513581862af256d67ebfae19bb122fd9dd
timboy6699 <35691577+timboy6699@users.noreply.github.com>
2018-01-31 12:00:52 +0100

    javadoc

--- ParkingSimulator/src/hanze/project/logic/AdHocCar.java
+++ ParkingSimulator/src/hanze/project/logic/AdHocCar.java
@@ -3,29 +3,37 @@ package hanze.project.logic;
 import java.util.Random;
 import java.awt.*;
 
-// CLASS
+/**
+ * Class AdHocCar
+ * Deze klasse breid de "Car" klasse uit en creëert de "Ad Hoc" auto's (normale auto's).
+ *
+ * @author Jurian de Vries, Sebastiaan ter Veen, Deni Grabic, Tim Gorter, Sander Steenbergen
+ * @version 31-01-2018
+ */
 
 public class AdHocCar extends Car {
 
-    // FIELDS
+    // De velden
 
 	private static final Color COLOR=Color.red;
 
-    // CONSTRUCTORS
+	// De constructors
 
     public AdHocCar() {
     	Random random = new Random();
     	int stayMinutes = (int) (15 + random.nextFloat() * 3 * 60);
-    	double teBetalenBedrag = stayMinutes * 0.02;
-    	this.setTotaalPrijs(teBetalenBedrag);
         this.setMinutesLeft(stayMinutes);
         this.setHasToPay(true);
         this.setHasReservation(false);
     }
 
-    // METHODS
+    /**
+     * Deze methode wordt gebruikt om de kleur van de auto terug te geven.
+     * @return COLOR De kleur van de auto
+     */
 
     public Color getColor(){
     	return COLOR;
     }
+
 }
