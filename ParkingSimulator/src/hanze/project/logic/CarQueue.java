f3b2c6ef48d72820f6d94b26ec8bf043b6603f23
timboy6699 <35691577+timboy6699@users.noreply.github.com>
2018-01-31 12:02:43 +0100

    javadoc

--- ParkingSimulator/src/hanze/project/logic/CarQueue.java
+++ ParkingSimulator/src/hanze/project/logic/CarQueue.java
@@ -3,27 +3,54 @@ package hanze.project.logic;
 import java.util.LinkedList;
 import java.util.Queue;
 
-// CLASS
+/**
+ * Class Car
+ * Deze klasse zorgt ervoor dat de auto's in de garage komen.
+ *
+ * @author Jurian de Vries, Sebastiaan ter Veen, Deni Grabic, Tim Gorter, Sander Steenbergen
+ * @version 31-01-2018
+ */
 
 public class CarQueue {
 
-    // FIELDS
+    // De velden
 
     private Queue<Car> queue = new LinkedList<>();
 
-    // METHODS
+    // De methodes
+
+    /**
+     * Voegt een auto toe aan de wachtrij.
+     * @param car Auto die toegevoegd moet worden.
+     * @return boolean Of de auto in de rij staat
+     */
 
     public boolean addCar(Car car) {
         return queue.add(car);
     }
 
+    /**
+     * Haalt auto weg uit de wachtrij.
+     * @return Car uit de wachtrij
+     */
+
     public Car removeCar() {
         return queue.poll();
     }
 
+    /**
+     * Geeft de lengte van de rij terug.
+     * @return int Lengte van de rij
+     */
+
     public int carsInQueue(){
     	return this.queue.size();
     }
+
+    /**
+     * Geeft de voorste auto in de wachtrij terug.
+     * @return Car voorste auto uit rij
+     */
 
     public Car frontCar(){
         return queue.peek();
